571817661be1b737dda94419d4573e3d91476fcc
zasrin <miamisbiggest@gmail.com>
2018-03-03 07:22:25 -0500

    model updates
    fixed json from model updates, included program.cs but it is excluded from csproj

---
+++ src/Feature/GDT.TrackingConsent/Program.cs
@@ -0,0 +1,49 @@
+﻿using GDT.TrackingConsent.Models;
+using Sitecore.XConnect.Client;
+using Sitecore.XConnect.Client.WebApi;
+using Sitecore.XConnect.Schema;
+using Sitecore.XConnect.Serialization;
+using Sitecore.Xdb.Common.Web;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GDT.TrackingConsent
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("");
+            Console.WriteLine("END OF PROGRAM.");
+            Console.ReadKey();
+        }
+
+        private static async Task MainAsync(string[] args)
+        {
+            CertificateWebRequestHandlerModifierOptions options =
+            CertificateWebRequestHandlerModifierOptions.Parse("StoreName=My;StoreLocation=LocalMachine;FindType=FindByThumbprint;FindValue=75980C7DD01B535796DEE0A8E682F28EB465536D");
+
+            var certificateModifier = new CertificateWebRequestHandlerModifier(options);
+
+            List<IHttpClientModifier> clientModifiers = new List<IHttpClientModifier>();
+            var timeoutClientModifier = new TimeoutHttpClientModifier(new TimeSpan(0, 0, 20));
+            clientModifiers.Add(timeoutClientModifier);
+
+            var collectionClient = new CollectionWebApiClient(new Uri("https://schackwishingwebz01xconnect/odata"), clientModifiers, new[] { certificateModifier });
+            var searchClient = new SearchWebApiClient(new Uri("https://schackwishingwebz01xconnect/odata"), clientModifiers, new[] { certificateModifier });
+            var configurationClient = new ConfigurationWebApiClient(new Uri("https://schackwishingwebz01xconnect/configuration"), clientModifiers, new[] { certificateModifier });
+
+            var cfg = new XConnectClientConfiguration(
+                new XdbRuntimeModel(ConsentModel.Model), collectionClient, searchClient, configurationClient);
+
+            var json = XdbModelWriter.Serialize(ConsentModel.Model);
+            Console.WriteLine(json);
+        }
+    }
+}
+        
